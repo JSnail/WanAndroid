5437bc92836e10b3d015721c3ee96961f63506f1
jiangrongbo <jiangrongbo35@auto-link.com.cn>
2020-12-30 21:25:26 +0800

    添加方法实现

--- app/src/main/java/com/snail/wanandroid/repository/HomeRepository.kt
+++ app/src/main/java/com/snail/wanandroid/repository/HomeRepository.kt
@@ -1,6 +1,7 @@
 package com.snail.wanandroid.repository
 
 import com.snail.wanandroid.api.ApiService
+import com.snail.wanandroid.entity.HomeAllEntity
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
@@ -10,8 +11,16 @@ class HomeRepository constructor(private val apiService: ApiService) {
 
     }
 
-    suspend fun getHomeAllData() = coroutineScope {
+    suspend fun getHomeAllData(): HomeAllEntity = coroutineScope {
         val banner = async { apiService.getHomeBanner() }
+        val topArticle = async { apiService.getArticleTop() }
+        val homeArticleList = async { apiService.getHomeArticleList(0) }
+
+        return@coroutineScope HomeAllEntity(
+            bannerEntity = banner.await().recordset,
+            articleTopEntity = topArticle.await().recordset,
+            articleListEntity = homeArticleList.await().recordset
+        )
     }
 
 }
